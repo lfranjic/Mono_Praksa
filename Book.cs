c24a8b2d854710be73571e068c291263d99a6252
lfranjic <83988340+lfranjic@users.noreply.github.com>
2023-01-16 17:34:08 +0100

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayOne
+{
+    public abstract class Book: IBook
+    {
+        string IBook.ToString => throw new NotImplementedException();
+        public new abstract string ToString { get; }
+    }
+}
