fcc961f30f4d9ba357a1f65e30de4f5db9b46203
lfranjic <leofranjic46@gmail.com>
2023-01-17 15:28:54 +0100

    Update

--- DayTwo/Book/Book.WebAPI/Controllers/BookController.cs
+++ DayTwo/Book/Book.WebAPI/Controllers/BookController.cs
@@ -42,22 +42,40 @@ namespace Book.WebAPI.Controllers
         }
 
         // POST api/<controller>
-        /*
-        public HttpResponseMessage Post([FromBody])
-        {
-            
-        }
-        */
-        // PUT api/<controller>/5
-        /*
-        public void Put(int id, [FromBody] )
+        // POST: api/Values
+        public HttpResponseMessage saveBook([FromBody] Book newBook)
         {
 
+            if (!books.Exists(Book => Book.Id == newBook.Id))
+            {
+                books.Add(newBook);
+                return Request.CreateResponse(HttpStatusCode.OK, "Added!");
+            }
+            else return Request.CreateResponse(HttpStatusCode.BadRequest, "Porduct with the same id exists!");
         }
-        */
-        // DELETE api/<controller>/5
-        public void Delete(int id)
+
+        [HttpPut]
+        // PUT: api/Values/5
+        public HttpResponseMessage changeGenre([FromUri] int id, [FromUri] string value)
         {
+            var foundBook = books.Find(Book => Book.Id == id);
+            foundBook.Genre = value;
+            if (foundBook != null)
+            {
+                if (foundBook.Genre == value) return Request.CreateResponse(HttpStatusCode.OK, "Changed!");
+                else return Request.CreateResponse(HttpStatusCode.NotModified, "Error!");
+            }
+            else return Request.CreateResponse(HttpStatusCode.NotFound, "Not found!");
+        }
+
+        [HttpDelete]
+        // DELETE: api/Values/5
+        public HttpResponseMessage removeBook([FromUri] int id)
+        {
+            int numberOfbooks = books.Count;
+            books.Remove(books.Find(Book => Book.Id == id));
+            if (books.Count == numberOfbooks - 1) return Request.CreateResponse(HttpStatusCode.OK, "Deleted!");
+            else return Request.CreateResponse(HttpStatusCode.NotModified, "Error!"); ;
         }
     }
 }
